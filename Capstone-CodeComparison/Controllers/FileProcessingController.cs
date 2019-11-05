8b186abae087a4ce232ad36a81c0c441e2751b0a
Rottman, Patrick J. (MU-Student) <pjrc74@mail.missouri.edu>
2019-11-05 00:38:00 -0600

    added basic interface and the begining of upload/download logic.

---
+++ Capstone-CodeComparison/Controllers/FileProcessingController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Net;
+using System.IO;
+
+namespace Capstone_CodeComparison.Controllers
+{
+    public class FileProcessingController : Controller
+    {
+        // GET: FileProcessing
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Upload()
+        {
+            try
+            {
+                foreach (string file in Request.Files)
+                {
+                    var fileContent = Request.Files[file];
+                    if (fileContent != null && fileContent.ContentLength > 0)
+                    {
+                        // get a stream
+                        var stream = fileContent.InputStream;
+                        var fileName = Path.GetFileName(file);
+                        
+                        byte[] data = new byte[stream.Length];
+                        int br = stream.Read(data, 0, data.Length);
+
+
+                        Session["FileContent"] = data;
+                        Session["FileContentName"] = fileName;
+
+                        //https://stackoverflow.com/questions/31914568/save-an-attachment-in-session
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return Json("Upload failed");
+            }
+
+            return Json("File uploaded successfully");
+        }
+
+        public ActionResult Download()
+        {
+            if (Session["FileContent"] != null)
+            {
+                return File(Session["FileContent"] as byte[], System.Net.Mime.MediaTypeNames.Application.Octet, Session["FileContentName"].ToString());
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+    }
+}
--- Capstone-CodeComparison/Controllers/HomeController.cs
+++ Capstone-CodeComparison/Controllers/HomeController.cs
@@ -10,6 +10,7 @@ namespace Capstone_CodeComparison.Controllers
     {
         public ActionResult Index()
         {
+            ViewBag.LanguageDropDownList = new SelectList(new List<String> {"C#", "HTML", "C", "Python"});
             return View();
         }
 
